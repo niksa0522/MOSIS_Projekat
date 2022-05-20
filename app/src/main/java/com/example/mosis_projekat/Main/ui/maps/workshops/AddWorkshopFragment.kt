1acf4b10c69d9e4cd56dfb5e38522dadf3e43190
NikolaPraksa <nikola.ilic.bs@elfak.rs>
2022-05-20 09:56:00 +0200

    Krenuto sa dodavanjem lokacije

---
+++ app/src/main/java/com/example/mosis_projekat/Main/ui/maps/workshops/AddWorkshopFragment.kt
@@ -0,0 +1,50 @@
+package com.example.mosis_projekat.Main.ui.maps.workshops
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.viewModels
+import com.example.mosis_projekat.R
+import com.example.mosis_projekat.databinding.FragmentAddWorkshopBinding
+import com.example.mosis_projekat.databinding.FragmentHomeBinding
+
+
+class AddWorkshopFragment : Fragment() {
+
+
+    private var _binding: FragmentAddWorkshopBinding? = null
+
+    private val AddViewModel: AddWorkshopViewModel by viewModels()
+
+    // This property is only valid between onCreateView and
+    // onDestroyView.
+    private val binding get() = _binding!!
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+    }
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        // Inflate the layout for this fragment
+
+        _binding = FragmentAddWorkshopBinding.inflate(inflater, container, false)
+        val root: View = binding.root
+
+        return root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+    }
+
+    override fun onDestroyView() {
+        super.onDestroyView()
+        _binding = null
+    }
+
+}
---
+++ app/src/main/java/com/example/mosis_projekat/Main/ui/maps/workshops/AddWorkshopViewModel.kt
@@ -0,0 +1,6 @@
+package com.example.mosis_projekat.Main.ui.maps.workshops
+
+import androidx.lifecycle.ViewModel
+
+class AddWorkshopViewModel : ViewModel() {
+}
